2f7dc7c531a3d7995b38b70d2533f5499b1a8548
Hai LE Manh <hai.lemanh.1310@gmail.com>
2022-02-24 11:09:18 +0700

    update banner

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <?php require "template-parts/layouts/header.php"; ?>
 <div class="uk-section home__section">
     <div class="uk-container">
+        <!--home__block01-->
         <div class="uk-section-xsmall home__block01">
             <ul class="home__block01__subnav uk-subnav uk-subnav-divider uk-flex-center" uk-margin>
                 <li><a href="#">Sản phẩm bán chạy</a></li>
@@ -10,6 +11,9 @@
                 <li class="uk-active"><a href="#">Khuyến mại sốc</a></li>
             </ul>
         </div>
+        <!--/home__block01-->
+
+        <!--home__blockSlider-->
         <div class="home__blockSlider uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="ratio: 1021:382">
 
             <ul class="uk-slideshow-items">
@@ -25,47 +29,50 @@
             <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
 
         </div>
-        <div class="uk-section-xsmall">
+        <!--/home__blockSlider-->
+
+        <!--home__block02-->
+        <div class="home__block02 uk-section-xsmall">
             <div class="uk-grid-divider uk-grid-small uk-child-width-1-2 uk-child-width-1-4@m" uk-grid>
                 <div>
-                    <div class="uk-flex-middle uk-grid-small" uk-grid>
+                    <div class="uk-flex-middle uk-grid-small uk-flex-center" uk-grid>
                         <div class="uk-width-auto">
                             <div class="home__block02__icon" data-src="images/1x/icon_ship.png" uk-img></div>
                         </div>
-                        <div class="uk-width-expand">
+                        <div class="uk-width-auto@m" uk-toggle="cls: uk-width-expand; mode: media; media: (max-width: 959.8px)">
                             <div class="home__block02__txt1">Miễn phí giao hàng</div>
                             <div class="home__block02__txt2">cho đơn hàng từ 500k</div>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div class="uk-flex-middle uk-grid-small" uk-grid>
+                    <div class="uk-flex-middle uk-grid-small uk-flex-center" uk-grid>
                         <div class="uk-width-auto">
                             <div class="home__block02__icon" data-src="images/1x/icon_uudai.png" uk-img></div>
                         </div>
-                        <div class="uk-width-expand">
+                        <div class="uk-width-auto@m" uk-toggle="cls: uk-width-expand; mode: media; media: (max-width: 959.8px)">
                             <div class="home__block02__txt1">Nhận nhiều ưu đãi</div>
                             <div class="home__block02__txt2">Đăng ký thành viên</div>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div class="uk-flex-middle uk-grid-small" uk-grid>
+                    <div class="uk-flex-middle uk-grid-small uk-flex-center" uk-grid>
                         <div class="uk-width-auto">
                             <div class="home__block02__icon" data-src="images/1x/icon_qua.png" uk-img></div>
                         </div>
-                        <div class="uk-width-expand">
+                        <div class="uk-width-auto@m" uk-toggle="cls: uk-width-expand; mode: media; media: (max-width: 959.8px)">
                             <div class="home__block02__txt1">Giới thiệu nhận quà</div>
                             <div class="home__block02__txt2">Giới thiệu ngay</div>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div class="uk-flex-middle uk-grid-small" uk-grid>
+                    <div class="uk-flex-middle uk-grid-small uk-flex-center" uk-grid>
                         <div class="uk-width-auto">
                             <div class="home__block02__icon" data-src="images/1x/icon_hotro.png" uk-img></div>
                         </div>
-                        <div class="uk-width-expand">
+                        <div class="uk-width-auto@m" uk-toggle="cls: uk-width-expand; mode: media; media: (max-width: 959.8px)">
                             <div class="home__block02__txt1">Hỗ trợ trực tuyến</div>
                             <div class="home__block02__txt2">Từ 80AM - 11PM</div>
                         </div>
@@ -73,7 +80,38 @@
                 </div>
             </div>
         </div>
+        <!--/home__block02-->
 
+        <div>
+            <div class="home__item">
+                <div class="uk-child-width-1-3@m uk-grid-small" uk-grid>
+                    <div>
+                        <div class="uk-cover-container uk-border-rounded">
+                            <img src="images/1x/banner1.jpg" alt="" uk-cover="">
+                            <canvas width="321" height="142"></canvas>
+                            <a href="" class="uk-position-cover"></a>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-cover-container uk-border-rounded">
+                            <img src="images/1x/banner2.jpg" alt="" uk-cover="">
+                            <canvas width="321" height="142"></canvas>
+                            <a href="" class="uk-position-cover"></a>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-cover-container uk-border-rounded">
+                            <img src="images/1x/banner3.jpg" alt="" uk-cover="">
+                            <canvas width="321" height="142"></canvas>
+                            <a href="" class="uk-position-cover"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="home__item">
+
+            </div>
+        </div>
     </div>
 </div>
 <?php require "template-parts/layouts/footer.php"; ?>
